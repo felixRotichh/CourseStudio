53a34c8a5eb653b67a47004d3068f07ac55b0fc8
felixrotichh <felixkibet2000@gmail.com>
2024-12-12 10:52:48 +0300

    added lesson model

--- courses/admin.py
+++ courses/admin.py
@@ -1,24 +1,31 @@
+from cloudinary import CloudinaryImage
 from django.contrib import admin
-import logging
-from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 
 # Register your models here.
-from .models import Course
+from .models import Course, Lesson
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    readonly_fields = ['updated']
+    extra = 0
+
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
     list_display = ['title', 'status', 'access']
     list_filter = ['status', 'access']
     fields = ['title', 'description', 'status', 'image', 'access', 'display_image']
     readonly_fields = ['display_image']
 
-    logger = logging.getLogger(__name__)
     
     def display_image(self, obj, *args,**kwargs):
-        url = obj.image.url if obj.image else ''  # Check if image exists
-        logger.debug(f"Image URL: {url}")  # Log the URL for debugging
-        if not url:
-            return "No image available"  # Return a message if no image
-        return mark_safe(f"<img src='{url}' style='max-width: 100px;'/>")  # Use mark_safe to render HTML
+        url = obj.image_admin
+       
+        return format_html(f"<img src={url} />")
+    
+    display_image.short_description = "Current image"
+        
 #admin.site.register(Course)
 
--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 import helpers
+from django.utils.text import slugify
+import uuid
 from cloudinary.models import CloudinaryField
 
 helpers.cloudinary_init()
@@ -16,12 +18,32 @@ class AccessRequirement(models.TextChoices):
 
 def handle_upload(instance, filename):
     return f"{filename}"
+
+def get_public_id_prefix(instance,*args, **kwargs):
+    title = instance.title
+    if title:
+        slug = slugify(title)
+        unique_id = slugify(uuid.uuid4()).replace("-", "")[:5]
+        return f"courses/{slug}-{unique_id}"
+    if instance.id:
+        return f"courses/{instance.id}"
+    return "courses"
+
+def get_display_name(instance,*args, **kwargs):
+    title = instance.title
+    if title:
+        return title    
+    return "Courses Upload"
     
 class Course(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     #image = models.ImageField(upload_to=handle_upload, blank=True, null=True)
-    image = CloudinaryField("image", null=True)
+    image = CloudinaryField("image", null=True, 
+                            public_id_prefix=get_public_id_prefix, 
+                            display_name=get_display_name,
+                            tags=["course","thumbnail"]
+                            )
     access = models.CharField(
         max_length=35,
         choices=AccessRequirement.choices,
@@ -32,10 +54,57 @@ class Course(models.Model):
         choices=PublishStatus.choices,
         default=PublishStatus.DRAFT
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     @property
     def is_published(self):
         return self.status == PublishStatus.PUBLISHED
+    
+    @property 
+    def image_admin(self):
+        if not self.image:
+            return""
+        image_options = {
+            "width":200
+        }
+        url =  self.image.build_url(**image_options)
+        return url
+    
+    def get_image_detail(self, as_html=False, width=750):
+        if not self.image:
+            return""
+        image_options = {
+            "width":200
+        }
 
+        if as_html:
+            return self.image.image(**image_options)
+        url =  self.image.build_url(**image_options)
+        return
+    
+#Lesson.objects.alll() #lesson queryset -> all rows
+#lesson.objects.first()
+#course_obj = Course.objects.first()
+#Lesson.objects.filter(course__id =course_obj.id)
+#course_obj.lesson_set.all()
+#lesson_obj = lesson.objects.first()
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField("image", blank=True, null=True)
+    video = CloudinaryField("video", blank=True, null=True, resource_type="video")
+    order = models.IntegerField(default=0)
+    can_preview = models.BooleanField(default=False, help_text="If user does not have access course, can they see this?" )
+    status = models.CharField(
+        max_length=15,
+        choices=PublishStatus.choices,
+        default=PublishStatus.PUBLISHED
+    )
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['order', '-updated']
